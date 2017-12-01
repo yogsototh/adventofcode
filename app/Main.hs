30b9ea0217e487755df055b7b09b5e989da1c3d2
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2017-12-02 00:27:43 +0100

    clean exec

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Protolude
+import Text.PrettyPrint hiding ((<>))
+
+import qualified Day1
+
+showSol :: [Char] -> Doc -> IO ()
+showSol txt d = putText . toS . render $
+  text ("  " <> txt <> ":")
+  <+> d
 
 main :: IO ()
-main = someFunc
+main = do
+  putText "Day 1:"
+  showSol "Solution 1" (int (Day1.solution1 Day1.ex1code))
+  showSol "Solution 2" (int (Day1.solution2 Day1.ex1code))
